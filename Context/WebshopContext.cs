825b402b2eb479737ff1608b87ee9c0e87caa780
Molnár Balázs <molnarbalazs1226@gmail.com>
2024-11-07 14:59:02 +0100

    Add project files.

---
+++ Context/WebshopContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebshopApi.Entities;
+
+namespace WebshopApi.Context
+{
+    public class WebshopContext : DbContext
+    {
+        public WebshopContext(DbContextOptions<WebshopContext> options) : base(options)
+        { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace WebshopApi.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public virtual Customer Customer { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
